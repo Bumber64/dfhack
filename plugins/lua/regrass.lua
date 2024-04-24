dd2025c994a8a6914c8648b38a62249b0b13fa45
Ryan Williams <ryan.williams.1990@gmail.com>
2024-04-24 04:46:16 -0700

    Update regrass.lua

--- plugins/lua/regrass.lua
+++ plugins/lua/regrass.lua
@@ -1,6 +1,31 @@
 local _ENV = mkmodule('plugins.regrass')
 
 local argparse = require('argparse')
+local utils = require('utils')
+
+local function search_str(s)
+    return dfhack.upperCp437(dfhack.toSearchNormalized(s))
+end
+
+local function find_grass_idx(s)
+    local grasses = df.global.world.raws.plants.grasses
+
+    if #grasses == 0 then
+        return -1 --no grass raws, world not loaded
+    elseif s == '' then
+        local rng = dfhack.random.new()
+        return rng:random(#grasses)
+    end
+
+    local id_str = search_str(s)
+    for _,grass in ipairs(grasses) do
+        if search_str(grass.id) == id_str then
+            return grass.index
+        end
+    end
+
+    return -1
+end
 
 function parse_commandline(opts, pos_1, pos_2, args)
     local positionals = argparse.processArgsGetopt(args,
@@ -13,12 +38,20 @@ function parse_commandline(opts, pos_1, pos_2, args)
         {'z', 'zlevel', handler=function() opts.zlevel = true end},
         {'f', 'force', hasArg=true, handler=function(optarg)
             opts.force = true
-            print('force', optarg)
+            opts.forced_plant = find_grass_idx(optarg)
         end},
     })
 
-    for _,p in ipairs(positionals) do
-        print(_, p)
+    if #positionals > 2 then
+        qerror('Too many positionals!')
+    end
+
+    if positionals[1] then
+        utils.assign(pos_1, argparse.coords(positionals[1], 'pos_1', true))
+    end
+
+    if positionals[2] then
+        utils.assign(pos_2, argparse.coords(positionals[1], 'pos_2', true))
     end
 end
 
