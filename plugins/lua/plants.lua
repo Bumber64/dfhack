7183440a66dfa38f1d75f68b924cb8ba39f76bb4
Ryan Williams <ryan.williams.1990@gmail.com>
2024-05-13 09:26:18 -0700

    Update plants.lua

--- plugins/lua/plants.lua
+++ plugins/lua/plants.lua
@@ -48,6 +48,24 @@ local function build_filter(vec, s)
     end
 end
 
+local age_table =
+{
+    TREE = 120959, --sapling_to_tree_threshold
+}
+
+local function plant_age(s) --tree stage or numerical value
+    if tonumber(s) then
+        return argparse.nonnegativeInt(s, 'age')
+    end
+
+    local n = age_table[s:upper()]
+    if n then
+        return n
+    end
+
+    qerror('Invalid age: "'..s..'"')
+end
+
 function parse_commandline(opts, pos_1, pos_2, filter_vec, args)
     local positionals = argparse.processArgsGetopt(args,
     {
@@ -59,6 +77,8 @@ function parse_commandline(opts, pos_1, pos_2, filter_vec, args)
         {'p', 'saplings', handler=function() opts.saplings = true end},
         {'t', 'trees', handler=function() opts.trees = true end},
         {'d', 'dryrun', handler=function() opts.dry_run = true end},
+        {'a', 'age', hasArg=true, handler=function(optarg)
+            opts.age = plant_age(optarg) end},
         {'f', 'filter', hasArg=true, handler=function(optarg)
             build_filter(filter_vec, optarg) end},
         {'e', 'exclude', hasArg=true, handler=function(optarg)
